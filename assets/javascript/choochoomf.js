1ceae903700dd1ce37ada4253b3f3ffbc0e9b59e
Red Tail <redtail814@gmail.com>
2019-07-02 01:21:06 -0400

    Created Firebase event when adding new train to database, new row is displayed in html.

--- assets/javascript/choochoomf.js
+++ assets/javascript/choochoomf.js
@@ -46,3 +46,56 @@ $("#add-train-btn").on("click", function (event) {
     $("#start-input").val("");
     $("#rate-input").val("");
 });
+
+// Create Firebase event for adding train to the database and a row in the html when a user adds an entry
+database.ref().on("child_added", function (childSnapshot) {
+    console.log(childSnapshot.val());
+
+    // Store everything into a variable.
+    var trainName = childSnapshot.val().name;
+    var trainDest = childSnapshot.val().destination;
+    var trainStart = childSnapshot.val().start;
+    var trainRate = childSnapshot.val().rate;
+
+    // Train Info
+    console.log(trainName);
+    console.log(trainDest);
+    console.log(trainStart);
+    console.log(trainRate);
+
+    // Train Start (pushed back 1 year to make sure it comes before current time)
+    var trainStartConverted = moment(trainStart, "HH:mm").subtract(1, "years");
+    console.log(trainStartConverted);
+
+    // Variable to hold current time
+    var currentTime = moment();
+    console.log("Current time: " + moment(currentTime).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(trainStartConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % trainRate;
+    console.log(tRemainder);
+
+    // Minutes until train arrives
+    var minutesAway = trainRate - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + minutesAway);
+
+    // Next time train arrives displayed in standard time.
+    var nextArrival = moment().add(minutesAway, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextArrival).format("hh:mm A"));
+
+    // Creates the new row
+    var newRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(trainDest),
+        $("<td>").text(trainRate),
+        $("<td>").text(moment(nextArrival).format("hh:mm A")),
+        $("<td>").text(minutesAway)
+    );
+
+    // Append the new row to the table
+    $("#train-table > tbody").append(newRow);
+});
